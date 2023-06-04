7bb7b3756f935376464c5c905ad7c62157af45bb
pea <pranali180802@gmail.com>
2023-06-04 13:08:06 +0530

    Java exception handling explained in code

---
+++ HowToExceptions.java
@@ -0,0 +1,26 @@
+package com.learn.ExceptionHandling;
+
+//import java.util.Scanner;
+
+public class HowToExceptions {
+
+	public static void main(String[] args) {
+		//Scanner sc= new Scanner(System.in); 
+		//System.out.println("Enter the 2 values");
+	
+		int a=4;
+		int b=0;
+		//double c=a/b;
+		
+		try {
+			double c=a/b;
+			System.out.println("after");
+
+			}catch(ArithmeticException e){
+				System.out.println(e);
+		}finally {
+			System.out.println("This will always print");
+		}
+	}
+	
+}
